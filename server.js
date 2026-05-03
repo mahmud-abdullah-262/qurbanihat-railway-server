36ba86b560ae8fa02ba8ffed01356ef16614ed08
mahmud-abdullah-262 <abdullah624683@gmail.com>
2026-05-03 08:07:16 +0600

    server js added

---
+++ server.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
